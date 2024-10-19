b9ac684b9e4a86c40a87df25b7044a14647798e3
JayConT <157869556+JayConT@users.noreply.github.com>
2024-10-19 12:34:13 +0700

    Add: Fixes to detail screen

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
-import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/redetail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi: candiList[0]),
+      home: ReDetailScreen(candi: candiList[0]),
     );
   }
 }
--- wisata_candi/lib/screens/redetail_screen.dart
+++ wisata_candi/lib/screens/redetail_screen.dart
@@ -1,33 +1,33 @@
-import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 import 'package:wisata_candi/models/candi.dart';
 
-class DetailScreen extends StatelessWidget {
+class ReDetailScreen extends StatelessWidget {
   final Candi candi;
-  const DetailScreen({super.key, required this.candi});
+  const ReDetailScreen({super.key, required this.candi});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Scaffold(
-        body: Column(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
             // Detail Header
             Stack(
+              // Main Image
               children: [
-                // Main Image
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
                   child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
                     child: Image.asset(
                       candi.imageAsset,
                       width: double.infinity,
                       height: 300,
                       fit: BoxFit.cover,
-                    )
+                    ),
                   ),
                 ),
+        
                 // Custom Back Button
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
@@ -40,7 +40,7 @@ class DetailScreen extends StatelessWidget {
                       onPressed: () {},
                       icon: const Icon(
                         Icons.arrow_back,
-                      )
+                      ),
                     ),
                   ),
                 )
@@ -53,7 +53,7 @@ class DetailScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(height: 16,),
-                  // Name and Favorites Button
+                  // Name and Favorite
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -66,54 +66,54 @@ class DetailScreen extends StatelessWidget {
                       ),
                       IconButton(
                         onPressed: () {},
-                        icon: Icon(Icons.favorite_border)
+                        icon: Icon(Icons.favorite_border),
                       )
                     ],
                   ),
-                  SizedBox(height: 16,),
                   // Location, Build Date, Type
+                  SizedBox(height: 16,),
                   Row(children: [
                     Icon(Icons.place, color: Colors.red,),
                     SizedBox(width: 8,),
                     SizedBox(
                       width: 70,
                       child: Text(
-                        'Lokasi',
-                        style: TextStyle(
+                        'Lokasi', style: TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                    Text(': ${candi.location}',),
-                  ],),
+                    Text(': ${candi.location}')
+                  ],
+                  ),
                   Row(children: [
                     Icon(Icons.calendar_month, color: Colors.blue,),
                     SizedBox(width: 8,),
                     SizedBox(
                       width: 70,
                       child: Text(
-                        'Dibangun',
-                        style: TextStyle(
+                        'Dibangun', style: TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                    Text(': ${candi.built}',),
-                  ],),
+                    Text(': ${candi.built}')
+                  ],
+                  ),
                   Row(children: [
                     Icon(Icons.house, color: Colors.green,),
                     SizedBox(width: 8,),
                     SizedBox(
                       width: 70,
                       child: Text(
-                        'Tipe',
-                        style: TextStyle(
+                        'Tipe', style: TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                    Text(': ${candi.type}',),
-                  ],),
+                    Text(': ${candi.type}')
+                  ],
+                  ),
                   SizedBox(height: 16,),
                   Divider(color: Colors.deepPurple.shade100,),
                   SizedBox(height: 16,),
@@ -128,9 +128,13 @@ class DetailScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Divider(color: Colors.deepPurple.shade100,),
-                  Text('Galeri', style: TextStyle(
-                    fontSize: 16, fontWeight: FontWeight.bold,
-                  ),),
+                  Text(
+                    'Galeri',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   SizedBox(height: 10,),
                   SizedBox(
                     height: 100,
@@ -162,7 +166,7 @@ class DetailScreen extends StatelessWidget {
                                     height: 120,
                                     color: Colors.deepPurple[50],
                                   ),
-                                  errorWidget: (context, url, error) => Icon(Icons.error),
+                                  errorWidget: (context, url, error,) => Icon(Icons.error),
                                 ),
                               ),
                             ),
@@ -172,12 +176,16 @@ class DetailScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 4,),
-                  Text('Tap untuk memperbesar', style: TextStyle(
-                    fontSize: 12, color: Colors.black54,
-                  ),),
+                  Text(
+                    'Tap untuk memperbesar',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black54,
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
